1b89ae542a6faa5abf3b21fa4a73e070c52e6139
ninfan <ninfan@users.noreply.github.com>
2016-01-24 21:27:15 +0100

    Create wlan.bat

---
+++ wlan.bat
@@ -0,0 +1,10 @@
+netsh wlan export profile folder=%~dp0\xml key=clear
+exit
+
+Zum Exportieren der Wlan Profile unter Windows
+Windows 7 Rechtsclick ausführen als Administrator
+Windows 10 im Administrator Konto ausführen.
+
+Ohne Admin keine entschlüsselung.
+
+
